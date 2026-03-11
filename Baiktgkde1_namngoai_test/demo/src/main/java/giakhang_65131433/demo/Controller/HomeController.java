e512cc07ca7b4f1198f0ff05817c4392d19878bf
khangprodepzai123 <khangnguyenquocgia@gmail.com>
2026-03-11 16:02:52 +0700

    hoan thien bai 2

--- Baiktgkde1_namngoai_test/demo/src/main/java/giakhang_65131433/demo/Controller/HomeController.java
+++ Baiktgkde1_namngoai_test/demo/src/main/java/giakhang_65131433/demo/Controller/HomeController.java
@@ -1,13 +1,34 @@
 package giakhang_65131433.demo.Controller;
+import java.util.Arrays;
+import java.util.List;
+
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+
+import giakhang_65131433.demo.Model.Page;
+import giakhang_65131433.demo.Model.Post;
 
 @Controller
 public class HomeController {
  
+    List<Page> pageList = Arrays.asList(
+        new Page(1, "Introduce", "about", "Nội dung giới thiệu", 0),
+        new Page(2, "Contact", "contact", "Nội dung liên hệ", 0),
+        new Page(3, "Admission", "admission", "Nội dung tuyển sinh", 1)
+    );
+
+    List<Post> postList = Arrays.asList(
+        new Post(1, "Bài viết 1", "Nội dung bài viết 1", 1),
+        new Post(2, "Bài viết 2", "Nội dung bài viết 2", 1),
+        new Post(3, "Bài viết 3", "Nội dung bài viết 3", 2)
+    );
+
     @GetMapping("/home")
-    public String dashboard() {
-    return "dashboard";
+    public String dashboard(Model model) {
+        model.addAttribute("pageList", pageList);
+        model.addAttribute("postList", postList);
+        return "dashboard";
     }
 }
     
