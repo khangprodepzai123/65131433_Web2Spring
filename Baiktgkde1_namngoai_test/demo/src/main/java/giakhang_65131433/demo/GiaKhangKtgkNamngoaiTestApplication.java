02284da2bbde0327d9d00fecc0d432923c29011b
khangprodepzai123 <khangnguyenquocgia@gmail.com>
2026-03-11 15:04:39 +0700

    baiktgiuakynamngoai

---
+++ Baiktgkde1_namngoai_test/demo/src/main/java/giakhang_65131433/demo/GiaKhangKtgkNamngoaiTestApplication.java
@@ -0,0 +1,13 @@
+package giakhang_65131433.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GiaKhangKtgkNamngoaiTestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GiaKhangKtgkNamngoaiTestApplication.class, args);
+	}
+
+}
---
+++ Baiktgkde1_namngoai_test/demo/src/test/java/giakhang_65131433/demo/GiaKhangKtgkNamngoaiTestApplicationTests.java
@@ -0,0 +1,13 @@
+package giakhang_65131433.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GiaKhangKtgkNamngoaiTestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
