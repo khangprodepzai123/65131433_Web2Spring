687fbbebb31144dc2ba9230ab6deb643c632a6e9
khangprodepzai123 <khangnguyenquocgia@gmail.com>
2026-04-07 15:02:52 +0700

    code giao dien dang ky dang nhap

---
+++ BaiQuanLyBanHang/src/main/java/giakhang/ntu65131433/qlbh/QuanLyBanHangApplication.java
@@ -0,0 +1,14 @@
+package giakhang.ntu65131433.qlbh;
+
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QuanLyBanHangApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(QuanLyBanHangApplication.class, args);
+	}
+
+}
---
+++ BaiQuanLyBanHang/src/test/java/giakhang/ntu65131433/qlbh/QuanLyBanHangApplicationTests.java
@@ -0,0 +1,13 @@
+package giakhang.ntu65131433.qlbh;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QuanLyBanHangApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
