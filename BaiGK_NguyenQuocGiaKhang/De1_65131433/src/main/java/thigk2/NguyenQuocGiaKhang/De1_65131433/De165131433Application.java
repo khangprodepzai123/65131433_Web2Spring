4a707a70b5332ed1bc67d9b8857776408ffad05c
khangprodepzai123 <khangnguyenquocgia@gmail.com>
2026-04-24 13:53:58 +0700

    build chuong trinh xay dung property-lan1

---
+++ BaiGK_NguyenQuocGiaKhang/De1_65131433/src/main/java/thigk2/NguyenQuocGiaKhang/De1_65131433/De165131433Application.java
@@ -0,0 +1,13 @@
+package thigk2.NguyenQuocGiaKhang.De1_65131433;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class De165131433Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(De165131433Application.class, args);
+	}
+
+}
---
+++ BaiGK_NguyenQuocGiaKhang/De1_65131433/src/test/java/thigk2/NguyenQuocGiaKhang/De1_65131433/De165131433ApplicationTests.java
@@ -0,0 +1,13 @@
+package thigk2.NguyenQuocGiaKhang.De1_65131433;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class De165131433ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
