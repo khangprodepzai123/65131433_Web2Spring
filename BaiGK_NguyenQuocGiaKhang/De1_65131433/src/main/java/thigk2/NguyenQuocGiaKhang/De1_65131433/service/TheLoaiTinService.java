dc0ef6214639b1885fd21f1a86dd39270f1b57eb
khangprodepzai123 <khangnguyenquocgia@gmail.com>
2026-04-24 14:24:51 +0700

    them depency- sua lai the loai tin service- push lan 6

--- BaiGK_NguyenQuocGiaKhang/De1_65131433/src/main/java/thigk2/NguyenQuocGiaKhang/De1_65131433/service/TheLoaiTinService.java
+++ BaiGK_NguyenQuocGiaKhang/De1_65131433/src/main/java/thigk2/NguyenQuocGiaKhang/De1_65131433/service/TheLoaiTinService.java
@@ -6,6 +6,7 @@ import thigk2.NguyenQuocGiaKhang.De1_65131433.repository.TheLoaiTinRepository;
 import thigk2.NguyenQuocGiaKhang.De1_65131433.repository.TinRepository;
 import org.springframework.stereotype.Service;
 import java.util.List;
+import java.util.Optional;
 
 @Service
 public class TheLoaiTinService {
@@ -23,7 +24,19 @@ public class TheLoaiTinService {
         return theLoaiTinRepository.findAll();
     }
 
+    public List<Tin> getAllTin() {
+        return tinRepository.findAll();
+    }
+
     public List<Tin> getTinByTheLoai(Integer theLoaiId) {
         return tinRepository.findByTheLoaiTinId(theLoaiId);
+    }
+
+    public Optional<Tin> getTinById(Integer id) {
+        return tinRepository.findById(id);
+    }
+
+    public Optional<TheLoaiTin> getTheLoaiById(Integer id) {
+        return theLoaiTinRepository.findById(id);
     }
 }
